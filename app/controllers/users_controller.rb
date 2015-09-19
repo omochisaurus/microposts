30f468b6ea861483b8d0fb411eb7cf7376aa7a3d
omochisaurus <omochisaurus@gamil.com>
2015-09-19 12:03:57 +0000

    編集機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+   @user = User.find(params[:id])
+  end   
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -19,10 +23,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+       @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user , notice: '情報を編集しました'
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :live, :profile)
   end
 end
