c4861ba80863280554e3f4218a3fcdf1ca44e072
omochisaurus <omochisaurus@gamil.com>
2015-10-03 08:04:25 +0000

    ruby修正

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,7 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
-      render 'static_pages/home'
+      redirect_to root_url
     end
   end
 
